3efc85ce6cf373725364657778c2b188cb9a175c
Vadim Y. Devyatnikov <68634043+vydevyatnikov@users.noreply.github.com>
2020-08-02 18:22:53 +0300

    Update Plot3.R

--- myplots/Plot3.R
+++ myplots/Plot3.R
@@ -5,8 +5,7 @@ data$Time <- as.POSIXlt(paste(as.character(data$Date), data$Time, sep = " "), fo
 
 #PLOT N3
 png(filename = "plot3.png")
-plot(data$Time, data$Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering")
+plot(data$Time, data$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
 lines(data[,c(2,8)], col = "red")
 lines(data[,c(2,9)], col = "blue")
-lines(data[,c(2,7)])
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, col = c("black", "red", "blue"))
